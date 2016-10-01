30516af733a21c8e670f01cf8b28ee28508301c2
Ruzicka Pavel <ruza@ruza.eu>
2016-10-01 21:00:24 +0200

    LOGFILE added for arp-localnet.sh

--- NeNe-discovery/arp-localnet.sh
+++ NeNe-discovery/arp-localnet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 IFACE_DEFAULT="$(/usr/local/bin/netiface-default.py)"
+DATE="$(date +%Y-%m-%d--%H:%M)"
+LOGFILE="/home/LEAKS/wifi/arp-scan/${DATE}-${IFACE_DEFAULT}.log"
 
-/usr/bin/arp-scan --interface=${IFACE_DEFAULT} --localnet
+/usr/bin/arp-scan --interface=${IFACE_DEFAULT} --localnet|tee "${LOGFILE}"
